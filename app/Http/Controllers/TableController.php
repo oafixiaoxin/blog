38ea5553186a8b84f9be8fc76f8199654b0dbb20
yanshuxin <359569219@qq.com>
2017-08-08 14:03:49 +0800

    update function sendOrder 7

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -123,6 +123,7 @@ WHERE 1=1 AND ta.`id`=?', [$id]);
 	    	$tableId = $request->input('tableId');
 	    	$mealNumber = $request->input('mealNumber');
 	    	$time = $request->input('time');
+	    	return $this->output(Response::SUCCESS, $selectedMenu);
 	    	
 	    	DB::beginTransaction();
 //	    	DB::table('mantadia_tables')->where('id', $tableId)->update(['status' => 1]);
