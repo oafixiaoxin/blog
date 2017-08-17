b0a08fb264d7a9f919097a3a24b33f420eaf2d52
yanshuxin <359569219@qq.com>
2017-08-17 16:53:16 +0800

    update 46

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -34,17 +34,12 @@
 	    			$image_name = date('YmdHis').time().'.'.$result[2];
 	    		}
 	 
-	    		if ( !file_exists('/www/wwwroot/image/upload/'.date('Ymd',time()).'/') )
-	    		{
-	    			mkdir('/www/wwwroot/image/upload/'.date('Ymd',time()).'/', 0700);
-	    		}
+//	    		if ( !file_exists('/www/wwwroot/image/upload/'.date('Ymd',time()).'/') )
+//	    		{
+//	    			mkdir('/www/wwwroot/image/upload/'.date('Ymd',time()).'/', 0700);
+//	    		}
 	    		$filepath = '/www/wwwroot/image/upload/'.date('Ymd',time()).'/'.$image_name;
-//	    		$tempAry = [
-//	    			"arg0" => $result[0],
-//	    			"arg1" => $result[1],
-//	    			"arg2" => $result[2],
-//	    			"arg3" => $filepath
-//	    		];
+
 	    		return $this->output(Response::SUCCESS, $filepath);
 //	    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], '', $base64_str))) )
 //	    		{
