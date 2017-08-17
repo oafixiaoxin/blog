ea9b0d76dd3fbb38bdbad7a80c3e0990381215b0
yanshuxin <359569219@qq.com>
2017-08-17 14:10:49 +0800

    update 10

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -20,25 +20,25 @@
 	    public function uploadImage ( Request $request )
 	    {
 	    	$base64_str = $request->input('imgBase64');
-	    	return $this->output(Response::SUCCESS, $base64_str);
-//	    	$base64_image = str_replace('', '+', $base64_str);
-//	    	if ( preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_str, $result) )
-//	    	{
-//	    		//匹配成功 
-//	    		if ( $result[2] == 'jpeg' )
-//	    		{
-//	    			$image_name = uniqid().'.jpg';
-//	    		}
-//	    		else
-//	    		{
-//	    			$image_name = uniqid().'.'.$result[2];
-//	    		}
-//	    		return $this->output(Response::SUCCESS, $image_name);
-//	    	}
-//	    	else
-//	    	{
-//	    		return $this->output(Response::NO_MORE_INFO);
-//	    	}
+//	    	return $this->output(Response::SUCCESS, $base64_str);
+	    	$base64_image = str_replace('', '+', $base64_str);
+	    	if ( preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_str, $result) )
+	    	{
+	    		//匹配成功 
+	    		if ( $result[2] == 'jpeg' )
+	    		{
+	    			$image_name = uniqid().'.jpg';
+	    		}
+	    		else
+	    		{
+	    			$image_name = uniqid().'.'.$result[2];
+	    		}
+	    		return $this->output(Response::SUCCESS, $image_name);
+	    	}
+	    	else
+	    	{
+	    		return $this->output(Response::NO_MORE_INFO);
+	    	}
 	    }
 	
 	}
