f83569d63eec9eb5304fa517de2ff0859dfdf5a2
yanshuxin <359569219@qq.com>
2017-08-17 15:17:01 +0800

    update 18

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -35,14 +35,20 @@
 	    			$image_name = date('YmdHis').time().'.'.$result[2];
 	    		}
 	    		$filepath = '../image/upload/'.$image_name;
-	    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], "", $base64_str))) )
-	    		{
-	    			return $this->output(Response::SUCCESS, $image_name);
-	    		}
-	    		else
-	    		{
-	    			return $this->output(Response::SAVE_IMG_FAILED);
-	    		}
+	    		$tempAry = [
+	    			"arg0" => $result[0];
+	    			"arg1" => $result[1];
+	    			"arg2" => $result[2];
+	    		];
+	    		return $this->output(Response::SUCCESS, $tempAry);
+//	    		if ( file_put_contents($filepath, base64_decode(str_replace($result[1], "", $base64_str))) )
+//	    		{
+//	    			return $this->output(Response::SUCCESS, $image_name);
+//	    		}
+//	    		else
+//	    		{
+//	    			return $this->output(Response::SAVE_IMG_FAILED);
+//	    		}
 	    	}
 	    	else
 	    	{
